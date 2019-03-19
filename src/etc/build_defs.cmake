586dbce593f66e6b3954901fefbd74dcce3f4408
kwantam <kwantam@gmail.com>
2019-03-19 11:56:18 -0700

    more build system simplification

--- src/etc/build_defs.cmake
+++ src/etc/build_defs.cmake
@@ -1,6 +1,8 @@
 find_library (LIBGMP gmp)
 find_library (LIBCRYPTO crypto)
 macro (add_bh_exec exec_name)
-    add_executable ("${exec_name}" "${exec_name}.c")
-    target_link_libraries ("${exec_name}" bhcommon bint "${LIBGMP}" "${LIBCRYPTO}" ${ARGN})
+    add_executable ("${exec_name}" "${exec_name}.c"
+                                   "${PROJECT_SOURCE_DIR}/bhcommon/common.c"
+                                   "${PROJECT_SOURCE_DIR}/bint/bint.c")
+    target_link_libraries ("${exec_name}" "${LIBGMP}" "${LIBCRYPTO}" ${ARGN})
 endmacro (add_bh_exec)
