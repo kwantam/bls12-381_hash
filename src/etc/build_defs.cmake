4248f65e22e88b2d09eae9deeb0deed2eb7e02d4
kwantam <kwantam@gmail.com>
2019-03-19 22:01:29 -0700

    build system: compile common files into static lib first

--- src/etc/build_defs.cmake
+++ src/etc/build_defs.cmake
@@ -1,10 +1,13 @@
+include_directories ("${PROJECT_SOURCE_DIR}/bint" "${PROJECT_SOURCE_DIR}/curve" "${PROJECT_SOURCE_DIR}/util")
+
+add_library (bls_hash STATIC "${PROJECT_SOURCE_DIR}/util/util.c"
+                             "${PROJECT_SOURCE_DIR}/bint/bint.c"
+                             "${PROJECT_SOURCE_DIR}/curve/curve.c")
+
 find_library (LIBGMP gmp)
 find_library (LIBCRYPTO crypto)
+
 macro (add_bh_exec exec_name)
-    add_executable ("${exec_name}" "${exec_name}.c"
-                                   "${PROJECT_SOURCE_DIR}/util/util.c"
-                                   "${PROJECT_SOURCE_DIR}/bint/bint.c"
-                                   "${PROJECT_SOURCE_DIR}/curve/curve.c"
-                                   )
-    target_link_libraries ("${exec_name}" "${LIBGMP}" "${LIBCRYPTO}" ${ARGN})
+    add_executable ("${exec_name}" "${exec_name}.c")
+    target_link_libraries ("${exec_name}" bls_hash "${LIBGMP}" "${LIBCRYPTO}" ${ARGN})
 endmacro (add_bh_exec)
