749032d748b202f962743e41c2a341d6a7090c00
kwantam <kwantam@gmail.com>
2019-03-24 17:09:22 -0700

    build system reorg: move main programs into 'apps'

--- src/etc/tests.cmake
+++ src/etc/tests.cmake
@@ -1,6 +1,6 @@
 enable_testing()
 
-add_test(NAME t_hash_and_try COMMAND "${PROJECT_SOURCE_DIR}/test/run_test.sh" "${CMAKE_BINARY_DIR}/hash_and_try" "${PROJECT_SOURCE_DIR}/test/test.sage" "try")
-add_test(NAME t_hash_svdw_1 COMMAND "${PROJECT_SOURCE_DIR}/test/run_test.sh" "${CMAKE_BINARY_DIR}/hash_svdw_1" "${PROJECT_SOURCE_DIR}/test/test.sage" "1")
-add_test(NAME t_hash_svdw_2 COMMAND "${PROJECT_SOURCE_DIR}/test/run_test.sh" "${CMAKE_BINARY_DIR}/hash_svdw_2" "${PROJECT_SOURCE_DIR}/test/test.sage" "2")
-add_test(NAME t_hash_svdw_rG COMMAND "${PROJECT_SOURCE_DIR}/test/run_test.sh" "${CMAKE_BINARY_DIR}/hash_svdw_rG" "${PROJECT_SOURCE_DIR}/test/test.sage" "rG")
+add_test(NAME t_hash_and_try COMMAND "${PROJECT_SOURCE_DIR}/test/run_test.sh" "${CMAKE_BINARY_DIR}/apps/hash_and_try" "${PROJECT_SOURCE_DIR}/test/test.sage" "try")
+add_test(NAME t_hash_svdw_1 COMMAND "${PROJECT_SOURCE_DIR}/test/run_test.sh" "${CMAKE_BINARY_DIR}/apps/hash_svdw_1" "${PROJECT_SOURCE_DIR}/test/test.sage" "1")
+add_test(NAME t_hash_svdw_2 COMMAND "${PROJECT_SOURCE_DIR}/test/run_test.sh" "${CMAKE_BINARY_DIR}/apps/hash_svdw_2" "${PROJECT_SOURCE_DIR}/test/test.sage" "2")
+add_test(NAME t_hash_svdw_rG COMMAND "${PROJECT_SOURCE_DIR}/test/run_test.sh" "${CMAKE_BINARY_DIR}/apps/hash_svdw_rG" "${PROJECT_SOURCE_DIR}/test/test.sage" "rG")
