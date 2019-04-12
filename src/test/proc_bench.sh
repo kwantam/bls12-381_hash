c7554184813d6ed439931e1f106eaa11e165411f
kwantam <kwantam@gmail.com>
2019-04-11 23:07:53 -0700

    update benchmark scripts and results table

--- src/test/proc_bench.sh
+++ src/test/proc_bench.sh
@@ -5,6 +5,10 @@
 set -e
 set -o pipefail
 
+. "$(dirname "$0")/bench_setup"
+DIV_VAL=$((1000 * ${NUM_RUNS}))
+RND_VAL=$((${DIV_VAL} / 2))
+
 NUM_BENCH=1
 mapfile bench1_vals < "$1"
 if [ ! -z "$2" ]; then
@@ -13,13 +17,13 @@ if [ ! -z "$2" ]; then
 fi
 
 
-if [ "${#bench1_vals[@]}" != 15 ] || ( [ "$NUM_BENCH" = "2" ] && [ "${#bench2_vals[@]}" != 15 ] ); then
-    echo "ERROR: input files must have 15 lines"
+if [ "${#bench1_vals[@]}" != 27 ] || ( [ "$NUM_BENCH" = "2" ] && [ "${#bench2_vals[@]}" != 27 ] ); then
+    echo "ERROR: input files must have 27 lines"
     exit 1
 fi
 
 div_round() {
-    echo $(($(($1 + 5000000000)) / 10000000000))
+    echo $(($(($1 + ${RND_VAL})) / ${DIV_VAL}))
 }
 
 show_q_fq_cq() {
@@ -45,44 +49,74 @@ show_q_fq_cq() {
 
 idx=0
 
-echo "Hash-and-check & --- &"
+echo "\$\\bm{G_1}\$ & Hash-and-check & --- &"
 show_q_fq_cq 1 0
 show_q_fq_cq 1 0
 show_q_fq_cq 0 1
-echo "\\midrule"
+echo "\\cmidrule{2-6}"
 
-echo "Construction \\#1 & \\S\\ref{sec:blsmap} &"
+echo "& Construction \\#1 & \\S\\ref{sec:blsmap} &"
 show_q_fq_cq 1 0
 show_q_fq_cq 1 0
 show_q_fq_cq 1 1
 echo "%"
 
-echo "& \\S\\ref{sec:blsmap2} &"
+echo "&& \\S\\ref{sec:blsmap2} &"
 show_q_fq_cq 0 0
 show_q_fq_cq 1 0
 show_q_fq_cq 1 1
-echo "\\midrule"
+echo "\\cmidrule{2-6}"
 
-echo "Construction \\#2 & \\S\\ref{sec:blsmap} &"
+echo "& Construction \\#2 & \\S\\ref{sec:blsmap} &"
 show_q_fq_cq 1 0
 show_q_fq_cq 1 0
 show_q_fq_cq 1 1
 echo "%"
 
-echo "& \\S\\ref{sec:blsmap2} &"
+echo "&& \\S\\ref{sec:blsmap2} &"
 show_q_fq_cq 0 0
 show_q_fq_cq 1 0
 show_q_fq_cq 1 1
-echo "\\midrule"
+echo "\\cmidrule{2-6}"
 
-echo "Construction \\#3 & \\S\\ref{sec:blsmap} &"
+echo "& Construction \\#3 & \\S\\ref{sec:blsmap} &"
 show_q_fq_cq 1 0
 show_q_fq_cq 1 0
 show_q_fq_cq 0 1
 echo "%"
 
-echo "& \\S\\ref{sec:blsmap2} &"
+echo "&& \\S\\ref{sec:blsmap2} &"
 show_q_fq_cq 0 0
 show_q_fq_cq 1 0
 show_q_fq_cq 0 1
+echo "\\midrule[\\heavyrulewidth]"
+
+echo "\$\\bm{G_2}\$ & Hash-and-check & --- &"
+show_q_fq_cq 1 0
+show_q_fq_cq 1 0
+show_q_fq_cq 0 1
+echo "\\cmidrule{2-6}"
+
+echo "& Construction \\#4 & \\S\\ref{sec:blsmap} &"
+show_q_fq_cq 1 0
+show_q_fq_cq 1 0
+show_q_fq_cq 1 1
+echo "%"
+
+echo "&& \\S\\ref{sec:blsmap2} &"
+show_q_fq_cq 0 0
+show_q_fq_cq 1 0
+show_q_fq_cq 1 1
+echo "\\cmidrule{2-6}"
+
+echo "& Construction \\#5 & \\S\\ref{sec:blsmap} &"
+show_q_fq_cq 1 0
+show_q_fq_cq 1 0
+show_q_fq_cq 1 1
+echo "%"
+
+echo "&& \\S\\ref{sec:blsmap2} &"
+show_q_fq_cq 0 0
+show_q_fq_cq 1 0
+show_q_fq_cq 1 1
 echo "%"
--- src/test/run_bench.sh
+++ src/test/run_bench.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-NUM_RUNS=10000000
-TESTS_hash_and_check=(q fq)
-TESTS_hash_svdw_1=(q fq cq)
-TESTS_hash_svdw_2=(q fq cq)
-TESTS_hash_svdw_rG=(q fq)
-TESTS_hash_swu_1=(q cq)
-TESTS_hash_swu_2=(q cq)
-TESTS_hash_swu_rG=(q)
-TESTS=(hash_and_check hash_svdw_1 hash_swu_1 hash_svdw_2 hash_swu_2 hash_svdw_rG hash_swu_rG)
-
 [ -z "$1" ] && { echo "Usage: $0 <apps_dir>"; exit 1; }
 
 set -e
 set -o pipefail
+
+. "$(dirname "$0")/bench_setup"
+
 rm -f "$1"/bench.out
 
 for app in "${TESTS[@]}"; do
